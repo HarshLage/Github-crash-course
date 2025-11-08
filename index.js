37ef55134ff1607f020010fdef7de93b122fe855
Harsh Rajendra Lage <harsh.lage@indexnine.com>
2025-11-08 19:44:56 +0530

    can i revet this too

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-// the things should be the same for thi
-
-function (l , b){
-    return l + b;
-}
-
-
-function something(l , b){
-    return l + b;
-}
-
-function there (){
-    return 445 + 45445;
-}
+// the things should be the same for this 
