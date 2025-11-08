894f298d7e9ee6cbfb47d0ab55ffd419a98bd0fb
Harsh Rajendra Lage <harsh.lage@indexnine.com>
2025-11-08 19:43:26 +0530

    this is the latest change done by the revert

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// the things should be the same for this 
+// the things should be the same for thi
+
+function (l , b){
+    return l + b;
+}
+
+
+function something(l , b){
+    return l + b;
+}
+
+function there (){
+    return 445 + 45445;
+}
